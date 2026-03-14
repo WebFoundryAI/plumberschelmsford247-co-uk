63ace1409e6a0b90f5408818d40bd93af634d39f
Steve Wyman <cloudventiresonline@gmail.com>
2026-03-14 09:56:58 +0000

    feat: customize with Chelmsford data and generate dynamic pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://manchesterblockeddrain.co.uk/',
+  site: 'https://plumberschelmsford247.co.uk/',
   integrations: [
     sitemap(),
     tailwind(),
